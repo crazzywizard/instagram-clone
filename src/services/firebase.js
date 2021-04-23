1a1999c63abf59f4acb8e26b70b544aaa8287060
sameer rasa <crazzywizard@gmail.com>
2021-04-22 23:46:11 -0400

    started timeline

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-import user from '../components/sidebar/user';
 import { FieldValue, db } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
@@ -51,4 +50,24 @@ export async function updateFollowedUserFollowers(
         ? FieldValue.arrayRemove(loggedInUserDocId)
         : FieldValue.arrayUnion(loggedInUserDocId)
     });
+}
+
+export async function getPhotos(userId, following) {
+  const result = await db.collection('photos').where('userId', 'in', following).get();
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }));
+  const photosWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true;
+      }
+      const user = await getUserByUserId(photo.userId);
+      const { username } = user[0];
+      return { username, ...photo, userLikedPhoto };
+    })
+  );
+  return photosWithUserDetails;
 }
